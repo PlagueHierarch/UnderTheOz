9891d2d4d11fd4fef66cf1829dce70373d47b817
송희준 <koreansongaji@gmail.com>
2024-08-17 01:19:37 +0900

    반경 5타일 내 적 접근 구현 완료

--- Assets/Scripts/PlayerScript/Player.cs
+++ Assets/Scripts/PlayerScript/Player.cs
@@ -13,15 +13,19 @@ public class Player : MovingObject
     [SerializeField] private GameObject _gameManagerObject;
     [SerializeField] private GameObject _playerAnimation;
     [SerializeField] private GameObject _camera;
+    [SerializeField] private GameObject _boomEffect;
 
     private Animator _animator;
     private GameManager _gameManager;
     private SpriteRenderer _animationSpriteRenderer;
     private DungeonGenerator.DungeonGenerator _dungeonGenerator;
     private BoardManager _boardManager;
+    private SpriteRenderer _boomSprite;
+    
 
     private void ReferenceComponent()
     {
+        _boomSprite = _boomEffect.GetComponent<SpriteRenderer>();
         _boardManager = GameObject.Find("BoardManager(Clone)").GetComponent<BoardManager>();
         _dungeonGenerator = GameObject.Find("BoardManager(Clone)").GetComponent<DungeonGenerator.DungeonGenerator>();
         _camera = GameObject.Find("Main Camera");
@@ -65,6 +69,10 @@ public class Player : MovingObject
         if (Input.GetKeyDown(KeyCode.Q))
         {
             RandomTeleport();
+        }
+        if (Input.GetKeyDown(KeyCode.W))
+        {
+            Bomb();
         }
     }
     private void InputKey() //8키 방향 이동 및 공격 함수
@@ -165,10 +173,38 @@ public class Player : MovingObject
         }
     }
 
-    private void RandomTeleport()
+    private void RandomTeleport() //텔레포트 스크롤 스크립트
     {
         Vector3Int randomPosisiton = _boardManager.RandomPosition(_dungeonGenerator.FloorPosition);
         transform.position = randomPosisiton;
+    }
+
+    private void Bomb() //반경 5타일 내에 20 데미지
+    {
+        Vector2 playerPosition = transform.position;
+        Vector2 rayPosition;
+
+        for(int x = -4; x <= 4; x++)
+        {
+            for(int y = -4; y <= 4; y++)
+            {
+                RaycastHit2D hit;
+                rayPosition = playerPosition + new Vector2(x, y);
+                hit = Physics2D.Linecast(rayPosition, rayPosition, blockingLayer);
+                if(hit.collider == null)
+                {
+                    continue;
+                }
+                else if(hit.collider.gameObject.CompareTag("Monster"))
+                {
+                    _boomEffect.GetComponent<Animator>().SetTrigger("isBoom");
+                    GameObject target = hit.collider.gameObject;
+                    target.GetComponent<Monster>(); //몬스터 체력 감소
+                    Debug.Log(target.gameObject.name + target.gameObject.transform.position + "폭탄 맞음");
+
+                }
+            }
+        }
     }
 
     protected override void OnCantMove<T>(T component) //벽이나 다른 물체에 막혔을때 호출
